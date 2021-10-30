9ac1b650577d4d0fdce9fb31d726c6e8accc8311
Ashwin S. Nair <58840757+Ashwin-exe@users.noreply.github.com>
2021-10-30 10:51:53 +0530

    leetcode

--- src/main/java/Array/ReverseStringIII.java
+++ src/main/java/Array/ReverseStringIII.java
@@ -3,35 +3,37 @@ package Array;
 public class ReverseStringIII {
 
         public static void main(String[] args) {
-            String s = "Oh god";
-            reverseString(s);
+            String s = "s'teL ekat edoCteeL tsetnoc";
+            System.out.println(reverseWords(s));
 //            System.out.println(s);
         }
-        public static void reverseString(String s) {
-            int start =0 , end = 1;
-            char[] charArray = s.toCharArray();
-            for (char ch : charArray) {
-                if(ch == ' ' ){
-                    while(start < end){
-                        swap(charArray,start,end);
-                        start++;
-                        end--;
-                    }
-                    end++;
-                    start = end;
-                }
-            }
-            for (char ch:charArray
-                 ) {
-                System.out.print(ch);
+    public static String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] charArray = s.toCharArray();
 
+        for(int startOfWord = 0;startOfWord < charArray.length; startOfWord++)
+        {
+            if(charArray[startOfWord] != ' ') // for non-space letters
+            {
+                int endOfWord =  startOfWord;//start of a new word
+                while(endOfWord + 1 < charArray.length && charArray[endOfWord+1] != ' ')
+                { endOfWord++; }//moving the pointer to the end of the word
+
+                reverse(charArray,startOfWord,endOfWord);//now reverse the whole word
+                startOfWord = endOfWord; // and move the pointer to end of the particular word
             }
         }
-        public static void swap(char[] ch,int start, int end){
+        return sb.append(charArray).toString();
+    }
+
+    public static void reverse(char[] ch,int start, int end){
+        while(start<end) {
             char temp = ch[start];
             ch[start] = ch[end];
             ch[end] = temp;
-
+            //now move both pointers inwards
+            start++;
+            end--;
         }
     }
-
+}
